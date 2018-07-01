b2fe7f8b05d178595b16ae224f2df6aa8830474c
Raoul v. R <vanruesc@users.noreply.github.com>
2018-07-01 23:00:03 +0200

    Version 1.0.2.

--- build/kukki.js
+++ build/kukki.js
@@ -1,6 +1,126 @@
 /**
- * kukki v1.0.1 build Fri Jun 29 2018
+ * kukki v1.0.2 build Sun Jul 01 2018
  * https://github.com/vanruesc/kukki
  * Copyright 2018 Raoul van Rüschen, Zlib
  */
-(function(a,b){'object'==typeof exports&&'undefined'!=typeof module?b(exports):'function'==typeof define&&define.amd?define(['exports'],b):b(a.KUKKI={})})(this,function(a){'use strict';var b=function(a,b){if(!(a instanceof b))throw new TypeError('Cannot call a class as a function')},c=function(){function a(a,b){for(var c,d=0;d<b.length;d++)c=b[d],c.enumerable=c.enumerable||!1,c.configurable=!0,'value'in c&&(c.writable=!0),Object.defineProperty(a,c.key,c)}return function(b,c,d){return c&&a(b.prototype,c),d&&a(b,d),b}}(),d=/^(?:expires|max-age|path|domain|secure)$/i,e=new Date(0),f=function(){function a(){b(this,a)}return c(a,null,[{key:'get',value:function(a){var b=null;return void 0!==a&&null!==a&&(b=decodeURIComponent(document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*'+encodeURIComponent(a).replace(/[-.+*]/g,'\\$&')+'\\s*=\\s*([^;]*).*$)|^.*$'),'$1'))),b}},{key:'set',value:function(a,b,c){if(c=Object.assign({expires:null,path:null,domain:null,secure:!1},c),void 0!==a&&null!==a&&!d.test(a)){var e=null===c.expires?'':'; expires='+c.expires.toUTCString(),f=null===c.path?'':'; path='+c.path,g=null===c.domain?'':'; domain='+c.domain,h=c.secure?'; secure':'';document.cookie=encodeURIComponent(a)+'='+encodeURIComponent(b)+e+f+g+h}}},{key:'has',value:function(a){var b=!1;return void 0===a||null===a||d.test(a)||(b=new RegExp('(?:^|;\\s*)'+encodeURIComponent(a).replace(/[-.+*]/g,'\\$&')+'\\s*=').test(document.cookie)),b}},{key:'delete',value:function(a,b){if(this.has(a)){b=Object.assign({path:null,domain:null},b);var c='; expires='+e.toUTCString(),d=null===b.path?'':'; path='+b.path,f=null===b.domain?'':'; domain='+b.domain;document.cookie=encodeURIComponent(a)+'='+c+d+f}}},{key:'keys',value:function(){return document.cookie.replace(/((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g,'').split(/\s*(?:=[^;]*)?;\s*/).map(function(a){return decodeURIComponent(a)})}}]),a}();a.Kukki=f,Object.defineProperty(a,'__esModule',{value:!0})});
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (factory((global.KUKKI = {})));
+}(this, (function (exports) { 'use strict';
+
+  var classCallCheck = function (instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  };
+
+  var createClass = function () {
+    function defineProperties(target, props) {
+      for (var i = 0; i < props.length; i++) {
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+      }
+    }
+
+    return function (Constructor, protoProps, staticProps) {
+      if (protoProps) defineProperties(Constructor.prototype, protoProps);
+      if (staticProps) defineProperties(Constructor, staticProps);
+      return Constructor;
+    };
+  }();
+
+  var cookieAttributeRegExp = /^(?:expires|max-age|path|domain|secure)$/i;
+
+  var past = new Date(0);
+
+  var Kukki = function () {
+  	function Kukki() {
+  		classCallCheck(this, Kukki);
+  	}
+
+  	createClass(Kukki, null, [{
+  		key: "get",
+  		value: function get$$1(key) {
+
+  			var result = null;
+
+  			if (key !== undefined && key !== null) {
+
+  				result = decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[-.+*]/g, "\\$&") + "\\s*=\\s*([^;]*).*$)|^.*$"), "$1"));
+  			}
+
+  			return result;
+  		}
+  	}, {
+  		key: "set",
+  		value: function set$$1(key, value, attributes) {
+
+  			attributes = Object.assign({
+  				expires: null,
+  				path: null,
+  				domain: null,
+  				secure: false
+  			}, attributes);
+
+  			if (key !== undefined && key !== null && !cookieAttributeRegExp.test(key)) {
+
+  				var expires = attributes.expires !== null ? "; expires=" + attributes.expires.toUTCString() : "";
+  				var path = attributes.path !== null ? "; path=" + attributes.path : "";
+  				var domain = attributes.domain !== null ? "; domain=" + attributes.domain : "";
+  				var secure = attributes.secure ? "; secure" : "";
+
+  				document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(value) + expires + path + domain + secure;
+  			}
+  		}
+  	}, {
+  		key: "has",
+  		value: function has(key) {
+
+  			var result = false;
+
+  			if (key !== undefined && key !== null && !cookieAttributeRegExp.test(key)) {
+
+  				result = new RegExp("(?:^|;\\s*)" + encodeURIComponent(key).replace(/[-.+*]/g, "\\$&") + "\\s*=").test(document.cookie);
+  			}
+
+  			return result;
+  		}
+  	}, {
+  		key: "delete",
+  		value: function _delete(key, attributes) {
+
+  			if (this.has(key)) {
+
+  				attributes = Object.assign({
+  					path: null,
+  					domain: null
+  				}, attributes);
+
+  				var expires = "; expires=" + past.toUTCString();
+  				var path = attributes.path !== null ? "; path=" + attributes.path : "";
+  				var domain = attributes.domain !== null ? "; domain=" + attributes.domain : "";
+
+  				document.cookie = encodeURIComponent(key) + "=" + expires + path + domain;
+  			}
+  		}
+  	}, {
+  		key: "keys",
+  		value: function keys() {
+
+  			return document.cookie.replace(/((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:=[^;]*)?;\s*/).map(function (key) {
+  				return decodeURIComponent(key);
+  			});
+  		}
+  	}]);
+  	return Kukki;
+  }();
+
+  exports.Kukki = Kukki;
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
