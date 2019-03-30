b27be0874dac29097a9b1d481e3ddedea0ae0fca
Raoul v. R <vanruesc@users.noreply.github.com>
2019-03-30 13:45:20 +0100

    Update rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -11,26 +11,28 @@ const banner = `/**
  * Copyright ${date.slice(-4)} ${pkg.author.name}, ${pkg.license}
  */`;
 
+const production = (process.env.NODE_ENV === "production");
+
 const lib = {
 
 	input: pkg.module,
 	output: {
-		file: "build/" + pkg.name + ".js",
+		file: pkg.main,
 		format: "umd",
 		name: pkg.name.replace(/-/g, "").toUpperCase(),
 		banner: banner
 	},
 
-	plugins: [resolve()].concat(process.env.NODE_ENV === "production" ? [babel()] : [])
+	plugins: [resolve()].concat(production ? [babel()] : [])
 
 };
 
-export default [lib].concat((process.env.NODE_ENV === "production") ? [
+export default [lib].concat(production ? [
 
 	Object.assign({}, lib, {
 
 		output: Object.assign({}, lib.output, {
-			file: "build/" + pkg.name + ".min.js"
+			file: pkg.main.replace(".js", ".min.js")
 		}),
 
 		plugins: [resolve(), babel(), minify({
